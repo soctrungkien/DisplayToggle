cab9e3ad0d9c93461e2f08cbf13e8d625b1235bf
TuiㅤKoㅤCóㅤTên <112756562+soctrungkien@users.noreply.github.com>
2026-02-08 18:38:43 +0700

    Refactor SetDisplayPowerModeActivity for power mode handling
    Refactor display power mode handling and use Thread for execution.

--- app/src/main/java/io/github/ulysseszh/displaytoggle/activity/SetDisplayPowerModeActivity.kt
+++ app/src/main/java/io/github/ulysseszh/displaytoggle/activity/SetDisplayPowerModeActivity.kt
@@ -4,36 +4,39 @@ import android.app.Activity
 import android.os.Bundle
 import android.util.Log
 import io.github.ulysseszh.displaytoggle.Constants.ACTION_SET_DISPLAY_POWER_MODE
-import io.github.ulysseszh.displaytoggle.ShizukuHelper
 import io.github.ulysseszh.displaytoggle.util.Display.PowerMode
 
-// One-off activity to set display power mode, needed because shortcuts cannot start services
 class SetDisplayPowerModeActivity : Activity() {
-	companion object {
-		private val TAG = SetDisplayPowerModeActivity::class.simpleName!!
-	}
 
-	private val shizukuHelper = ShizukuHelper(this)
-	private var powerMode: PowerMode? = null
+    companion object {
+        private val TAG = SetDisplayPowerModeActivity::class.simpleName!!
+    }
 
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		if (intent.action != ACTION_SET_DISPLAY_POWER_MODE) {
-			Log.e(TAG, "Unsupported action: ${intent.action}")
-			finish()
-			return
-		}
-		val powerModeString = intent.getStringExtra("POWER_MODE")
-		powerMode = PowerMode.fromString(powerModeString)
-		if (powerMode == null) {
-			Log.e(TAG, "Unsupported mode: $powerModeString")
-		} else {
-			shizukuHelper.addServiceConnectedListener {
-				shizukuHelper.setDisplayPowerMode(powerMode!!)
-				shizukuHelper.cleanUp()
-			}
-		}
-		shizukuHelper.setUp()
-		finish()
-	}
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (intent.action != ACTION_SET_DISPLAY_POWER_MODE) {
+            Log.e(TAG, "Unsupported action: ${intent.action}")
+            finish()
+            return
+        }
+
+        val powerModeString = intent.getStringExtra("POWER_MODE")
+        val powerMode = PowerMode.fromString(powerModeString)
+
+        if (powerMode == null) {
+            Log.e(TAG, "Unsupported mode: $powerModeString")
+            finish()
+            return
+        }
+
+        Thread {
+            when (powerMode) {
+                PowerMode.OFF -> ScreenController.turnOff()
+                PowerMode.ON -> ScreenController.turnOn()
+                else -> Log.e(TAG, "Unhandled power mode: $powerMode")
+            }
+            finish()
+        }.start()
+    }
 }
